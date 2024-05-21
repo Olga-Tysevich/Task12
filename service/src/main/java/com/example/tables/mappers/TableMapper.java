006f6ff11df0f3671259919ade7e8cd44978b849
Olga Tysevich <Olga.Tisevich@gmail.com>
2024-05-21 15:00:37 +0300

    .

--- service/src/main/java/com/example/tables/mappers/TableMapper.java
+++ service/src/main/java/com/example/tables/mappers/TableMapper.java
@@ -2,10 +2,7 @@ package com.example.tables.mappers;
 
 import com.example.tables.dto.TableDTO;
 import com.example.tables.entity.Table;
-import lombok.Lombok;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -15,13 +12,6 @@ public interface TableMapper {
 
     TableMapper INSTANCE = Mappers.getMapper(TableMapper.class);
 
-    @Mappings({
-            @Mapping(source = "id", target = "id"),
-            @Mapping(source = "brand", target = "brand"),
-            @Mapping(source = "color", target = "color"),
-            @Mapping(source = "material", target = "material"),
-            @Mapping(source = "size", target = "size")
-    })
     Table toEntity(TableDTO tableDTO);
 
     TableDTO toDTO(Table table);
